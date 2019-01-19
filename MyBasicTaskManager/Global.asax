a1dc0dc022d8c5cd7246e4dcb436ee4bfced1530
Tumidaju <Tumidaju@DESKTOP-HLB30V3>
2019-01-19 08:47:29 +0100

    Initial commit of ASP.Net application (MVC with silge user auth).

---
+++ MyBasicTaskManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyBasicTaskManager.MvcApplication" Language="C#" %>
---
+++ MyBasicTaskManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBasicTaskManager.Startup))]
+namespace MyBasicTaskManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
