067bedb732628f755766da5c9c9abc59cdbca4db
Tumidaju <Tumidaju@DESKTOP-HLB30V3>
2019-01-28 01:29:32 +0100

    Selenium test nugget
    Added selenium test driver for firefox

--- MyBasicTaskManager.Tests/SeleniumTests/Selenium.cs
+++ MyBasicTaskManager.Tests/SeleniumTests/Selenium.cs
@@ -16,7 +16,7 @@ namespace SeleniumDemo
             var text = "Pierwszy test"; _driver.Navigate().GoToUrl("https://google.pl");
             var searchTextBox = _driver.FindElement(By.Name("q"));
             searchTextBox.SendKeys(text + Keys.Enter);
-            new WebDriverWait(_driver, TimeSpan.FromSeconds(10)).Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.CssSelector("di v.hdtb-mitem:nth-child(1)"))));
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(10)).Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists((By.CssSelector("div.hdtb-mitem:nth-child(1)"))));
             StringAssert.StartsWith("https://www.google.pl/search?", _driver.Url);
         }
         [TearDown] public void CloseBrowser() { _driver.Close(); }
